f2c8c937272396cbf3de557d34c195bfd3119468
VulcanWM <logmedha09@gmail.com>
2023-05-15 21:17:53 +0100

    added svg for user

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,11 +1,12 @@
-import { get_user_without_set } from "../../lib/database";
+// import { get_user_without_set } from "../../lib/database";
 
-export default async function handler(req, res) {
-  if (req.method == 'GET') {
-    const username = req.query['username']
-    const user = await get_user_without_set(username)
-    res.status(200).json({ success: true, msg: null, data: user });
-  } else {
-    res.status(200).json({ success: false, msg: "Send a GET request", data: null });
-  }
-}
+// export default async function handler(req, res) {
+//   if (req.method == 'GET') {
+//     const username = req.query['username']
+//     const user = await get_user_without_set(username)
+//     // res.status(200).json({ success: true, msg: null, data: user });
+//     res.send("<h2>Hello</h2>")
+//   } else {
+//     res.status(200).json({ success: false, msg: "Send a GET request", data: null });
+//   }
+// }
