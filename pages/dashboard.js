f2bea2087178a0b9a13bfa2fd7cdb14237938b13
VulcanWM <logmedha09@gmail.com>
2023-05-15 20:55:10 +0100

    changed style for done todos

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -58,6 +58,7 @@ export default function Home( { user } ) {
     text.id = index + "title"
     text.style.display = 'inline'
     text.style.cursor = "pointer"
+    text.style.textDecoration = all_todos[index].done ? "line-through" : "none"
     input_el.parentNode.replaceChild(text, input_el);
   }
 
@@ -160,9 +161,9 @@ export default function Home( { user } ) {
       {all_todos.length>=20?<></>:
       <><input type="text" placeholder="New todo.." onKeyDown={addtodoFront}/><br/><br/></>}
       {Object.keys(all_todos).map((index) => (
-        <div key={index}>
+        <div key={index} className={all_todos[index].done ? "fontdarker" : ""}>
           <label id={index + "check"}><input checked={all_todos[index].done} id={index + "checkbox"} type='checkbox' onClick={() => checkboxClick(index)}/></label>
-          <h4 id={index + "title"} style={{display: "inline",cursor: "pointer", textDecoration: all_todos[index].done ? "line-through" : "none"}} onClick={() => clickRow(index)}>{all_todos[index].title} </h4>
+          <h4 id={index + "title"} style={{display: "inline",cursor: "pointer", textDecoration: all_todos[index].done ? "line-through" : "none"}}>{all_todos[index].title} </h4>
           <FontAwesomeIcon id={index + "edit"} onClick={() => clickTitle(index)} icon={faPencil} style={{width: "13px", height: "13px", cursor:"pointer"}}/>&#xA0;
           <FontAwesomeIcon id={index + "delete"} onClick={() => deleteTodo(all_todos[index].title)} icon={faTrashCan} style={{width: "13px", height: "13px", cursor:"pointer"}}/><br/><br/>
         </div>
